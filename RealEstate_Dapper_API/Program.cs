4f36107096042061f64f1fc99e4e4650ccc200c7
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-08-13 01:19:03 +0300

    API tarafında get isteginde bulunup categorilerin listesi geldi

--- RealEstate_Dapper_API/Program.cs
+++ RealEstate_Dapper_API/Program.cs
@@ -1,6 +1,11 @@
+using RealEstate_Dapper_API.Models.DapperContext;
+using RealEstate_Dapper_API.Repositories.CategoryRepository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<Context>();
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
