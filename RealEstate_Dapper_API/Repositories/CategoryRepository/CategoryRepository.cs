3897bd96b2bfecfd22737ee8a133d1586052eafe
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-08-13 00:49:46 +0300

    categori tablosunun dtoları ve db nin baglantisi olusturuldu

---
+++ RealEstate_Dapper_API/Repositories/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using RealEstate_Dapper_API.Dtos.CategoryDtos;
+using RealEstate_Dapper_API.Models.DapperContext;
+
+namespace RealEstate_Dapper_API.Repositories.CategoryRepository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly Context _context;
+
+        public CategoryRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            string query = "Select * from Category";
+            using (var connection=_context.CreateConnection())
+            {
+                var values=await connection.QueryAsync<ResultCategoryDto>(query);
+                return values.ToList(); 
+            }
+        }
+    } 
+}
---
+++ RealEstate_Dapper_API/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using RealEstate_Dapper_API.Dtos.CategoryDtos;
+
+namespace RealEstate_Dapper_API.Repositories.CategoryRepository
+{
+    public interface ICategoryRepository
+    {
+
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync(); 
+
+    }
+}
