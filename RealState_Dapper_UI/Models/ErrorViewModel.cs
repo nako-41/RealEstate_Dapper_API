981029f5cd5f95957e0ce661590a981806cb0ed7
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-08-13 00:10:55 +0300

    Add project files.

---
+++ RealState_Dapper_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealState_Dapper_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
