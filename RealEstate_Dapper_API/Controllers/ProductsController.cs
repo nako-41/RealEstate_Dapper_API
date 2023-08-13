aa099535454888f26c0d1e8d1322d7ee87f9b553
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-08-13 16:08:08 +0300

    iliskili olan product ve category tablosunu API tarafında get istegiyle cektim

---
+++ RealEstate_Dapper_API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_API.Repositories.CategoryRepository;
+using RealEstate_Dapper_API.Repositories.ProductRepository;
+
+namespace RealEstate_Dapper_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> ProductList()
+        {
+
+            var values = await _productRepository.GetAllProductAsync();
+            return Ok(values);
+
+        }
+        [HttpGet("ProductListWithCategory")]
+        public async Task<ActionResult> ProductListWithCategory()
+        {
+
+            var values = await _productRepository.GetAllProductWithCategory();
+            return Ok(values);
+
+        }
+    }
+}
